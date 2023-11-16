10a290137f57f10ec059769b706c3da2a5f42494
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-16 15:44:40 -0500

    create NewsArticles type file and import into  ArticleList.tsx  seems like an easier way to provide type info to the component     rather than using the interface in the component itself

---
+++ src/app/types/NewsArticles.ts
@@ -0,0 +1,9 @@
+// types/NewsArticle.ts
+export interface NewsArticle {
+  article_id: string;
+  title: string;
+  description: string;
+  link: string;
+  pubDate: string;
+  // Add more fields from your news data as needed
+}
