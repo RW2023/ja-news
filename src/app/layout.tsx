7936485e4d25f0019a8151b6d0553c33b6cea15d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-17 10:28:42 -0500

    remove loading bar effect in an effort to troubleshoot a diployment issue, I am removing the loading bar effect from the app.  I will add it back in later.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body>
         <Navbar />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
