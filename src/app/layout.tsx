a349ba17851139cda63ddcebbcbb089ee2b5ccd2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-27 18:39:20 -0500

    Add title to HTML document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
         />
         <link rel="icon" href="/favicon/favicon.ico" />
+        <title>Ja-News Aggregator</title>
       </head>
       <body>
         <Navbar />
